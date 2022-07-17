8fc936f0ce0484306ead8009b5b25d0795ace1d4
zukhriddinov <zukhr1dd1nov1312@gmail.com>
2022-07-17 20:56:27 +0500

    ...

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import PostModel
 
-# Create your views here.
+
+class PostListView(ListView):
+    template_name = 'blog.html'
+
+    def get_queryset(self):
+        qs = PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
+
+
+class PostDetailView(DetailView):
+    model = PostModel
+    template_name = 'blog-detail.html'
